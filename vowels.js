d1fd3b1cbe77562f4e129ab47c536c42c97a8e14
anniezh12 <aniqaelahi@yahoo.com>
2018-10-11 18:17:10 -0700

    using regex to find number of vowels

--- vowels.js
+++ vowels.js
@@ -12,3 +12,18 @@ function numberOfVowels(str)
   return counter;
 }
 numberOfVowels("hello a i oaeouu")
+
+//Second Method
+function numberOfVowels2(str)
+{
+
+  let newArray = str.match(/[aeiou]/gi) //g will take care of all the occurences in the string and i will take care of lower and uppercase letters
+  if(newArray !== null)
+  {
+    return newArray.length
+  }
+  else
+  return "No vowels Found"
+
+}
+numberOfVowels2("hpyyyl")
